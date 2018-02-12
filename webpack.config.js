fcf7c9e4d033226434c1f1937b068087fb00f608
Benjamin Clark <benjaminclark@Benjamins-Mac-mini.local>
2018-02-13 07:05:18 +1030

    babel and uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
 const path = require('path');
 const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+require("babel-polyfill");
 
 module.exports = {
     context: __dirname,
     entry: {
-        main: './src/index.js'
+        main: ['babel-polyfill', './src/index.js']
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    plugins: [ 
+    plugins: [
         new CleanWebpackPlugin(['dist']),
         // Home page
         new HtmlWebpackPlugin({
@@ -23,36 +25,47 @@ module.exports = {
         new webpack.optimize.CommonsChunkPlugin({
             name: 'common' // Specify the common bundle's name.
         }),
+        new UglifyJSPlugin()
     ],
     resolve: {
-      modules: [
-        path.resolve('./src'),
-        path.resolve('./node_modules')
-      ]
+        modules: [
+            path.resolve('./src'),
+            path.resolve('./node_modules')
+        ]
     },
     devtool: 'inline-source-map',
     devServer: { contentBase: './dist' },
     module: {
         rules: [
             {
-            test: /\.(scss)$/,
-            use: [{
-                loader: 'style-loader', // inject CSS to page
-            }, {
-                loader: 'css-loader', // translates CSS into CommonJS modules
-            }, {
-                loader: 'postcss-loader', // Run post css actions
-                options: {
-                plugins: function () { // post css plugins, can be exported to postcss.config.js
-                    return [
-                    require('precss'),
-                    require('autoprefixer')
-                    ];
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
-                }
-            }, {
-                loader: 'sass-loader' // compiles SASS to CSS
-            }]
+            },
+            {
+                test: /\.(scss)$/,
+                use: [{
+                    loader: 'style-loader', // inject CSS to page
+                }, {
+                    loader: 'css-loader', // translates CSS into CommonJS modules
+                }, {
+                    loader: 'postcss-loader', // Run post css actions
+                    options: {
+                        plugins: function () { // post css plugins, can be exported to postcss.config.js
+                            return [
+                                require('precss'),
+                                require('autoprefixer')
+                            ];
+                        }
+                    }
+                }, {
+                    loader: 'sass-loader' // compiles SASS to CSS
+                }]
             }
         ]
     }
